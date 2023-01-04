78d8016b19920212bab8dbbf40b1468ce7e09180
Jerin Peter George <jerinpetergeorge@gmail.com>
2023-01-04 09:55:27 +0530

    Add main module

---
+++ django_app_conf/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for Django App Config."""
+
+__author__ = """Jerin Peter George"""
+__email__ = 'jerinpetergeorge@gmail.com'
+__version__ = '0.0.1'
+
+from .settings import AppSettings
+
+__all__ = ['AppSettings']
